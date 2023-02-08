7d054858caab3b7695e9f01ce7336cf9269095ef
Ben Swan <swanben223@gmail.com>
2023-02-08 15:41:45 +0000

    Added MouseLook script

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -9,10 +9,12 @@ public class MouseLook : MonoBehaviour
 
     public Transform playerBody;
 
+    float xRotation = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -21,6 +23,10 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xrotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
