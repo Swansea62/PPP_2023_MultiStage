86d77dd604ed58f1530d1a40edad3c46a10ed813
Ben Swan <swanben223@gmail.com>
2023-02-13 14:36:37 +0000

    amending movement script

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -13,8 +13,8 @@ public class Movement : MonoBehaviour
         float x = Input.GetAxis("Horziontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x * transform.forward * z;
+        Vector3 move = transform.right * x + transform.forward * z;
 
-        controller.Mpove(move * speed * Time.deltaTime);
+        controller.Move(move * speed * Time.deltaTime);
     }
 }
