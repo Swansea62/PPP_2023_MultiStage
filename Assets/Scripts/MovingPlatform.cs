d8af1a9a698a5e00da5503d87187118c0394c5b6
Ben Swan <swanben223@gmail.com>
2023-03-08 15:09:50 +0000

    Added moving platforms with scripts

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private WaypointPath waypointPath;
+
+    [SerializeField]
+    private float speed;
+
+    private int targetWaypointIndex;
+
+    private Transform previousWaypoint;
+    private Transform targetWaypoint;
+
+    private float timeToWaypoint;
+    private float elapsedTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TargetNextWaypoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        float elapsedPercentage = elapsedTime / timeToWaypoint;
+        transform.position = Vector3.Lerp(previousWaypoint.position, targetWaypoint.position, elapsedPercentage);
+
+        if (elapsedPercentage >= 1)
+        {
+            TargetNextWaypoint();
+        }
+    }
+
+    private void TargetNextWaypoint()
+    {
+        previousWaypoint = waypointPath.GetWaypoint(targetWaypointIndex);
+        targetWaypointIndex = waypointPath.GetNextWayPoint(targetWaypointIndex);
+        targetWaypoint = waypointPath.GetWaypoint(targetWaypointIndex);
+
+        elapsedTime = 0;
+
+        float distanceToWaypoint = Vector3.Distance(previousWaypoint.position, targetWaypoint.position);
+        timeToWaypoint = distanceToWaypoint / speed;
+    }
+}
