edd621404955ac9f0a5f9c2a0864f4a4ad7a1397
Ben Swan <swanben223@gmail.com>
2023-04-10 16:02:32 +0100

    Added new Skybox to all Menu Scenes. Also attempted to move stopwatch text upon level completion, but isn't working.

---
+++ Assets/Scripts/StopwatchPos.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StopwatchPos : MonoBehaviour
+{
+    private TextMeshProUGUI stopwatchText;
+
+    /*void OnTriggerEnter()
+    {
+        if ()
+        {
+            stopwatchText.transform.position = new Vector3(473.0f, -236.5f, 0.0f);
+        }
+    }*/
+}
